7ff531aa831d55a31e4a36978b20f441e3570568
Dominicus Ariel Christianto <arielchristianto@ti.ukdw.ac.id>
2017-03-25 17:33:20 +0700

    FFT DONE

--- app/src/js/core/fourierTransform.js
+++ app/src/js/core/fourierTransform.js
@@ -1,15 +1,20 @@
+import math from 'mathjs';
+
 const fourier = (function () {
+    let calculateSubFft = function (sequence) {
+
+    };
     return {
         calculateDft: function (sequence) {
             let result = [];
             let N = sequence.length;
 
-            for (let k=0; k<N; k++) {
+            for (let k = 0; k < N; k++) {
                 let res = math.complex(0, 0);
 
-                for(let n = 0; n< N; n++) {
-                    let real = math.cos(2*math.pi*k*n/N);
-                    let imaginer = -1 * math.sin(2*math.pi*k*n/N);
+                for (let n = 0; n < N; n++) {
+                    let real = math.cos(2 * math.pi * k * n / N);
+                    let imaginer = -1 * math.sin(2 * math.pi * k * n / N);
                     let complex = math.complex(real, imaginer);
 
                     res = math.add(res, math.multiply(sequence[n], complex));
@@ -19,16 +24,43 @@ const fourier = (function () {
             }
         },
 
-        calculateAmplitude: function (sequence) {
+        calculateFft: function (sequence) {
+            if (length === 1) {
+                return sequence;
+            }
 
-        },
+            let halfLength = length/2;
 
-        calculatePhase: function () {
+            let evenFft = [];
+            let oddFft = [];
+            evenFft.length = Math.round(halfLength);
+            oddFft.length = Math.round(halfLength);
 
-        },
+            for(let i=0; i<halfLength; i++) {
+                evenFft[i] = sequence[i * 2];
+                oddFft[i] = sequence[i * 2 + 1];
+            }
+            evenFft = calculateFft(evenFft);
+            oddFft = calculateFft(oddFft);
 
-        calculateFft: function () {
-            //todo: do this shit
+            //merging
+            for(let k = 0; k<halfLength; k++) {
+                let wReal = Math.cos(2 * math.pi * k / length);
+                let wImaginer = -1 * Math.sin(2 * math.pi * k / length);
+                let W = math.complex(wReal, wImaginer);
+
+                sequence[k] = math.format(math.add(
+                    evenFft[k],
+                    math.multiply(oddFft[k], W)
+                ), 2);
+
+                sequence[k+halfLength] = math.format(math.add(
+                    evenFft[k],
+                    math.multiply(math.multiply(oddFft[k], W), -1)
+                ), 2);
+            }
+
+            return sequence;
         }
     }
 
